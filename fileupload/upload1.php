7ec7723f4fceaca47f6c3a591bf391e9bdb5e79a
Ayush Kumar <68781382+Ares225@users.noreply.github.com>
2020-07-29 21:47:49 +0530

    Add files via upload

---
+++ fileupload/upload1.php
@@ -0,0 +1,38 @@
+<?php
+$name = $_FILES['file']['name'];
+$extension = strtolower(substr($name, strpos($name, '.') + 1));
+//$size = $_FILES['file']['size'];
+$type = $_FILES['file']['type'];
+
+$tmp_name = $_FILES['file']['tmp_name'];
+
+if(isset($name)) 
+{
+  if(!empty($name))
+   {
+    if (($extension=='jpg' || $extension=='jpeg')&& ($type=='image/jpeg'))
+     {
+      $location = 'uploads/';
+       if(move_uploaded_file($tmp_name, $location.$name))
+        {
+          echo 'Uploaded!';
+        } else
+        {
+           echo 'There was an error.';
+        }
+      } else
+      {
+           echo 'File must be jpg/jpeg.';
+      }
+     } else
+     {
+           echo 'Please choose a file.';
+     }
+
+  }
+?>
+
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+<input type="file" name="file"><br><br>
+<input type="submit" value="Submit">
+</form>
